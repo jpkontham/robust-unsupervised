935d01e4c6402d82c0e588ee1e049d418b207409
jpkontham <jpburself221005@gmail.com>
2026-04-12 17:28:33 +0530

    Update prelude.py

--- robust_unsupervised/prelude.py
+++ robust_unsupervised/prelude.py
@@ -1,8 +1,6 @@
 from typing import *
-
 import copy
 import os
-
 import functools
 import sys
 import torch.optim as optim
@@ -15,9 +13,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 import math
-import dnnlib as dnnlib
-import dnnlib.legacy as legacy
-# import legacy
 import shutil
 from functools import partial
 import itertools
@@ -26,7 +21,13 @@ from warnings import warn
 import datetime
 import torchvision.transforms.functional as TF
 from torchvision.utils import save_image, make_grid
-import training.networks as networks
+
+# --- THE CHANGE IS HERE ---
+# Instead of 'import dnnlib', we import from your new subfolder
+import stylegan3_ada.dnnlib as dnnlib
+import stylegan3_ada.legacy as legacy
+import stylegan3_ada.training.networks_stylegan3 as networks
+# ---------------------------
 
 from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
 from dataclasses import dataclass, field
@@ -34,9 +35,9 @@ from dataclasses import dataclass, field
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Standard Global for your 5-day sprint
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 warnings.filterwarnings("ignore", r"Named tensors and all their associated APIs.*")
 warnings.filterwarnings("ignore", r"Arguments other than a weight enum.*")
 warnings.filterwarnings("ignore", r"The parameter 'pretrained' is deprecated.*")
-
-
-
